a106d9c428783898d5094b7f42121d4349346fd0
sarath01sarath01 <sarath01sarath01@gmail.com>
2024-05-30 22:54:22 +0530

    added tooltip to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,8 @@ import React from "react";
 import Chip from "@mui/material/Chip";
 import classes from "./Card.module.css";
 import { Box, ThemeProvider, createTheme } from "@mui/system";
+import Tooltip from '@mui/material/Tooltip';
+
 
 // const theme = createTheme({
 //   palette: {
@@ -18,8 +20,10 @@ function Card({ data }) {
   let image = data.image
   let count = data.follows ? data.follows+" Follows" : data.likes+" Likes"
   let name = data.title
+  let title = data.follows ? data.songs.length+" Songs" : ""
   return (
     // <ThemeProvider theme={theme}>
+    <Tooltip title={title} placement="top" arrow>
       <div className={classes.card}>
         <div className={classes.container}>
           <img className={classes["card-img"]} src={image} alt="#" />
@@ -29,7 +33,7 @@ function Card({ data }) {
         </div>
         <div className={classes["card-name"]}>{name}</div>
       </div>
-    // </ThemeProvider>
+    </Tooltip>
   );
 }
 
